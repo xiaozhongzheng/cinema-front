8e6e1e401515c1d37b3624fcdfa7ac075db5ac29
xiaozhongzheng <151722392+xiaozhongzheng@users.noreply.github.com>
2025-12-01 16:51:51 +0800

    Add comment in vite.config.ts
    Add comment for remote Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import pxtorem from 'postcss-pxtorem'
-
+// 远程-vite配置
 export default defineConfig({
   plugins: [vue()],
   resolve: {
